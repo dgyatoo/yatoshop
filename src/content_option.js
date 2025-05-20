07665e45a1ba69f73ef15a7c480592fd769ca706
dgyatoo <zawngyato@gmail.com>
2025-05-20 16:28:56 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,71 @@
+const logotext = "YATO";
+const meta = {
+    title: "Yato",
+    description: "I’m Yato, a full stack developer, currently living in aizawl mizoram",
+};
+
+const introdata = {
+    title: "I’m Yato",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop whatsapp bot",
+    },
+    description: "🌹LEGALLY WRONG, ETHICALLY RIGHT 🌹",
+    your_img_url: "https://file.btch.rf.gd/file/i5m3fchcemnytsifw7i9.jpg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "i'm from Mizoram my name is yato im 18 year old.",
+};
+
+const skills = [{
+        name: "Html",
+        value: 90,
+    },
+    {
+        name: "Css",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "node.js",
+        value: 85,
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "yatoshop@gmail.com",
+    YOUR_FONE: "917630843657",
+    description: "🌹LEGALLY WRONG, ETHICALLY RIGHT 🌹.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+
+export {
+    meta,
+    dataabout,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
